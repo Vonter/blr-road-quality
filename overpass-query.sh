ad1637bf9db0659c9f38857a4ea797d29c1cb545
Vonter <25414711+Vonter@users.noreply.github.com>
2022-08-02 22:13:38 +0530

    GitHub Actions for automatically deploying site daily with latest OSM data

--- overpass-query.sh
+++ overpass-query.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 rm blr-road-quality.json blr-road-quality.geojson
-curl --output blr-road-quality.json "https://overpass-api.de/api/interpreter" --data "[out:json][timeout:10]; way["smoothness"](12,77,14,79); out geom;"
+
+while true;
+do
+    curl --output blr-road-quality.json "https://overpass-api.de/api/interpreter" --data "[out:json][timeout:10]; way["smoothness"](12,77,14,79); out geom;"
+    if find blr-road-quality.json -type f -size +51200c 2>/dev/null | grep -q .; then
+        echo "The overpass query has run successfully"
+        break
+    else
+        echo "The overpass query has failed... re-running in 15 seconds..."
+        sleep 15
+        continue
+    fi
+done
+
 osmtogeojson blr-road-quality.json > blr-road-quality.geojson.orig
 echo $(jq -c . < blr-road-quality.geojson.orig) > blr-road-quality.geojson
