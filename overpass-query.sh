b09722c0dc8e6d9bdae792ecb922b53cbf6ab289
Vonter <25414711+Vonter@users.noreply.github.com>
2022-08-04 00:49:23 +0530

    Add filter for flood prone roads

--- overpass-query.sh
+++ overpass-query.sh
@@ -4,7 +4,7 @@ rm blr-road-quality.json blr-road-quality.geojson
 
 while true;
 do
-    curl --output blr-road-quality.json "https://overpass-api.de/api/interpreter" --data "[out:json][timeout:10]; way["smoothness"](12,77,14,79); out geom;"
+    curl --output blr-road-quality.json "https://overpass-api.de/api/interpreter" --data "[out:json][timeout:10]; (way["smoothness"](12,77,14,79); way["flood_prone"](12,77,14,79);); out geom;"
     if find blr-road-quality.json -type f -size +51200c 2>/dev/null | grep -q .; then
         echo "The overpass query has run successfully"
         break
